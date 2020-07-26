7a906c9d9649adaf80c39a344f271d2175d77ae9
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2020-07-26 18:30:39 +0900

    Add files via upload

---
+++ reminder/todo.js
@@ -0,0 +1,7 @@
+
+var input = document.querySelector('.reminder_remind form input');
+var ul = document.querySelector('.reminder_remind ul');
+// 로컬스토리지 저장
+// 텍
+var p = document.createElement('p');
+input.value='asd';
